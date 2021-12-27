391c1d8d65781caafb89cbd005c8992cb0e4006f
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2021-12-27 15:56:11 +0530

    bitmanipulation

---
+++ BITMANIPULATION_PROBLEMS/342_Power of Four.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n==1)
+            return true;
+        if(n<=0)
+            return 0;
+        if(n%4!=0)
+            return 0;
+        return isPowerOfFour(n/4);
+        
+    }
+};
