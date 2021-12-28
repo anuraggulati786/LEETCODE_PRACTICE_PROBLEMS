8452aaf6e801588c772cb9f091ec2b677b83dfaa
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2021-12-28 15:54:40 +0530

    ARRAY

---
+++ ARRAY_LEETCODE_PROBLEMS/122_Best Time to Buy and Sell Stock II,cpp.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int max_profit=0;
+        for(int i=1;i<prices.size();i++)
+        {
+            if(prices[i]>prices[i-1])
+            {
+                max_profit+=(prices[i]-prices[i-1]);
+            }
+        }
+        if(max_profit<=0)
+            return 0;
+        return max_profit;
+        
+    }
+};
