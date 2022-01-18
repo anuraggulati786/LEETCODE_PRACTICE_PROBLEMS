64b5aa0fc43df3d0ced01dd911fa7ce19adbe958
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2022-01-18 20:42:57 +0530

    2-pointer code added

--- ARRAY_LEETCODE_PROBLEMS/283. Move Zeroes.cpp
+++ ARRAY_LEETCODE_PROBLEMS/283. Move Zeroes.cpp
@@ -10,3 +10,27 @@ public:
         }
     }
 };
+
+
+
+### two pionter approch ######
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int i;
+        int j=0;
+        int n=nums.size();
+        
+        for(i=0; i<n; i++) {
+            
+            if(nums[i]!=0) {
+                swap(nums[j],nums[i]);
+                j++;
+            }
+            
+        } 
+        
+    }
+};
