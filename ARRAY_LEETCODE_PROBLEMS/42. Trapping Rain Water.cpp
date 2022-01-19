41069004b47193ba2df26e1da5d5ea3a74acec9b
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2022-01-19 15:58:39 +0530

    using Dynamic programming

---
+++ ARRAY_LEETCODE_PROBLEMS/42. Trapping Rain Water.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int> right(n);
+        vector<int> left(n);
+        
+        if(n==0) {
+            return 0;
+        }
+        
+        
+        left[0] = height[0];
+        for(int i=1; i<n; i++) {
+            left[i] = max(height[i] , left[i-1]);
+        }
+        right[n-1] = height[n-1];
+        for(int i=n-2; i>=0; i--) {
+            right[i] = max(height[i] , right[i+1]);
+        }
+        int ans=0;
+        for(int i=0; i<n; i++) {
+            ans = ans + (min(left[i],right[i]) - height[i]);
+        }
+        return ans;
+    }
+};
