c03d6c6cb99067d2a5d2b8eb6c4585bea73b61bc
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2022-02-01 21:43:54 +0530

    matrix problems

---
+++ MATRIX/74. Search a 2D Matrix.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int start =0;
+        int end = row*col -1;
+        int mid = start + (end-start)/2;
+        
+        while(start <= end) {
+            mid = start + (end-start)/2;
+            
+            int element = matrix[mid/col][mid%col];
+            if(element == target) {
+                return 1;
+            }
+            else if(element > target) {
+                end = mid-1;
+            }
+            else {
+                start = mid+1;
+            }
+         
+        }
+        return 0;
+    }
+};
